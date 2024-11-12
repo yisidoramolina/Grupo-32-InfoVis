685afb1b97d538411249a947f9a03841602091a4
yisidoramolina <127432196+yisidoramolina@users.noreply.github.com>
2024-11-11 22:38:47 -0300

    d

--- script.js
+++ script.js
@@ -34,27 +34,29 @@ function stopAllApplause() {
 }
 
 const songs = [
-    { song: "Midnight Dreams", artist: "Luna Night", Spotify: 1200000, YouTube: 2500000, TikTok: 800000, explicit: "yes" },
-    { song: "Echoes of You", artist: "Echo Star", Spotify: 850000, YouTube: 1300000, TikTok: 2000000, explicit: "no" },
-    { song: "Faded Lights", artist: "Nova Sky", Spotify: 1000000, YouTube: 3500000, TikTok: 1200000, explicit: "yes" },
-    { song: "Ocean Breeze", artist: "Sunny Day", Spotify: 500000, YouTube: 900000, TikTok: 2500000, explicit: "no" },
-    { song: "Shadow Play", artist: "The Shades", Spotify: 700000, YouTube: 800000, TikTok: 1500000, explicit: "yes" },
-    { song: "Golden Hour", artist: "Ray Gold", Spotify: 4000000, YouTube: 1100000, TikTok: 500000, explicit: "no" },
-    { song: "Lost in Time", artist: "Chronos", Spotify: 600000, YouTube: 7000000, TikTok: 1000000, explicit: "yes" },
-    { song: "Neon City", artist: "The Streets", Spotify: 1300000, YouTube: 4500000, TikTok: 600000, explicit: "no" },
-    { song: "Electric Dreams", artist: "Zara Volt", Spotify: 3000000, YouTube: 2000000, TikTok: 3000000, explicit: "yes" },
-    { song: "Fire & Ice", artist: "Inferno & Frost", Spotify: 750000, YouTube: 2500000, TikTok: 1200000, explicit: "no" },
-    { song: "Dancing Shadows", artist: "Lily Mist", Spotify: 900000, YouTube: 3000000, TikTok: 2500000, explicit: "yes" },
-    { song: "Whispers in the Dark", artist: "Night Whisper", Spotify: 5000000, YouTube: 800000, TikTok: 1500000, explicit: "no" },
-    { song: "Runaway", artist: "Freedom Wave", Spotify: 850000, YouTube: 1100000, TikTok: 5500000, explicit: "yes" },
-    { song: "Forever Young", artist: "Youth", Spotify: 1500000, YouTube: 2500000, TikTok: 1000000, explicit: "no" },
-    { song: "High Voltage", artist: "Electric Pulse", Spotify: 1200000, YouTube: 3200000, TikTok: 900000, explicit: "yes" },
-    { song: "Into the Wild", artist: "Savanna", Spotify: 1100000, YouTube: 1000000, TikTok: 2000000, explicit: "no" },
-    { song: "Heartbeat", artist: "Soul Rhythm", Spotify: 950000, YouTube: 1200000, TikTok: 1500000, explicit: "yes" },
-    { song: "Golden Skies", artist: "Sunset Avenue", Spotify: 2500000, YouTube: 2000000, TikTok: 1800000, explicit: "no" },
-    { song: "Waves of Emotion", artist: "Sea Star", Spotify: 800000, YouTube: 600000, TikTok: 7000000, explicit: "yes" },
-    { song: "City Lights", artist: "Urban Flow", Spotify: 1500000, YouTube: 1800000, TikTok: 1200000, explicit: "no" }
+    { song: "As It Was", artist: "Harry Styles", Spotify: 6100000, YouTube: 1300000, TikTok: 2500000, explicit: "no" },
+    { song: "Running Up That Hill (A Deal With God)", artist: "Kate Bush", Spotify: 5000000, YouTube: 4000000, TikTok: 3000000, explicit: "no" },
+    { song: "Flowers", artist: "Miley Cyrus", Spotify: 4500000, YouTube: 3500000, TikTok: 2000000, explicit: "no" },
+    { song: "Kill Bill", artist: "SZA", Spotify: 4000000, YouTube: 3000000, TikTok: 1500000, explicit: "yes" },
+    { song: "Ella Baila Sola", artist: "Eslabon Armado & Peso Pluma", Spotify: 3500000, YouTube: 2500000, TikTok: 1000000, explicit: "yes" },
+    { song: "Creepin'", artist: "Metro Boomin, The Weeknd & 21 Savage", Spotify: 3000000, YouTube: 2000000, TikTok: 500000, explicit: "yes" },
+    { song: "La Bebe", artist: "Yng Lvcas & Peso Pluma", Spotify: 2500000, YouTube: 1500000, TikTok: 1000000, explicit: "yes" },
+    { song: "Calm Down", artist: "Rema & Selena Gomez", Spotify: 2000000, YouTube: 1000000, TikTok: 500000, explicit: "no" },
+    { song: "Unholy", artist: "Sam Smith & Kim Petras", Spotify: 1500000, YouTube: 500000, TikTok: 200000, explicit: "yes" },
+    { song: "Die For You", artist: "The Weeknd & Ariana Grande", Spotify: 1000000, YouTube: 400000, TikTok: 100000, explicit: "no" },
+    { song: "Fortnight", artist: "Taylor Swift & Post Malone", Spotify: 6100000, YouTube: 1300000, TikTok: 2500000, explicit: "no" },
+    { song: "I Had Some Help", artist: "Post Malone & Morgan Wallen", Spotify: 5000000, YouTube: 4000000, TikTok: 3000000, explicit: "no" },
+    { song: "Like That", artist: "Future, Metro Boomin & Kendrick Lamar", Spotify: 4500000, YouTube: 3500000, TikTok: 2000000, explicit: "yes" },
+    { song: "Texas Hold 'Em", artist: "Beyoncé", Spotify: 4000000, YouTube: 3000000, TikTok: 1500000, explicit: "yes" },
+    { song: "We Can't Be Friends", artist: "Ariana Grande", Spotify: 3500000, YouTube: 2500000, TikTok: 1000000, explicit: "no" },
+    { song: "Lose Control", artist: "Teddy Swims", Spotify: 3000000, YouTube: 2000000, TikTok: 500000, explicit: "yes" },
+    { song: "Sweet", artist: "Hozier", Spotify: 2500000, YouTube: 1500000, TikTok: 1000000, explicit: "no" },
+    { song: "Not Like Us", artist: "Kendrick Lamar", Spotify: 2000000, YouTube: 1000000, TikTok: 500000, explicit: "yes" },
+    { song: "Carnival", artist: "Kanye West & Ty Dolla $ign", Spotify: 1500000, YouTube: 500000, TikTok: 200000, explicit: "yes" },
+    { song: "Please Please Please", artist: "Sabrina Carpenter", Spotify: 1000000, YouTube: 400000, TikTok: 100000, explicit: "no" }
+    
 ];
+
 
 let explicitFilter = "all";
 
